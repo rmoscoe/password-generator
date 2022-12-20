1ade701d5815e8a64af2473cb51d370709b40f8d
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2022-12-19 16:21:11 -0800

    add JS code to enable and validate selection of character type(s)

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function generatePassword() {
   const lcLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   const ucLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
   const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  const specials = ["@", "%", "+", "\\", "/", "\'", "!", "#", "$", "^", "?", ":", ",", "(", ")", "{", "}", "[", "]", "~", "\`", "-", "_", "."];
+  const specials = ["@", "%", "+", "\\", "/", "\'", "!", "#", "$", "^", "?", ":", ",", "(", ")", "\)", "{", "}", "[", "]", "~", "\`", "-", "_", "."];
 
   //The following variables store the user's choices about the characters to be included in the password
   let pwLength = Number(prompt("Please choose a password length between 8 and 128 characters."));
@@ -34,5 +34,15 @@ function generatePassword() {
     pwLength = Number(prompt("Please choose a password length between 8 and 128 characters."));
     console.log(pwLength);
     console.log(Number(pwLength));
+  }
+
+  while (lowerCase === false && upperCase === false && numeric === false && special === false) {
+    lowerCase = confirm("Click OK to include lower case letters in your password; otherwise click Cancel.");
+    upperCase = confirm("Click OK to include upper case letters in your password; otherwise click Cancel.");
+    numeric = confirm("Click OK to include numbers (0-9) in your password; otherwise click Cancel.");
+    special = confirm("Click OK to include special characters (\"@%+\\/\'!#$^?:,(){}[]~\`-_.); otherwise click Cancel.");
+    if (lowerCase === false && upperCase === false && numeric === false && special === false) {
+      alert("You must select at least one type of character to include in your password.");
+    }
   }
 }
