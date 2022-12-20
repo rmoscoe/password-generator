152f550102e954493719fb2d84fae4a1b1326292
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2022-12-19 16:39:17 -0800

    add JS code to assemble array of all selected characters

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ function generatePassword() {
   const lcLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   const ucLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
   const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  //TODO: Figure out how to get the backslash to work
   const specials = ["@", "%", "+", "\\", "/", "\'", "!", "#", "$", "^", "?", ":", ",", "(", ")", "\)", "{", "}", "[", "]", "~", "\`", "-", "_", "."];
 
   //The following variables store the user's choices about the characters to be included in the password
   let pwLength = Number(prompt("Please choose a password length between 8 and 128 characters."));
-  console.log(pwLength);
-  console.log(Number(pwLength));
   let lowerCase = false;
   let upperCase = false;
   let numeric = false;
@@ -32,10 +32,9 @@ function generatePassword() {
   //Validate the user's chosen password length
   while (pwLength < 8 || pwLength > 128 || isNaN(pwLength)) {
     pwLength = Number(prompt("Please choose a password length between 8 and 128 characters."));
-    console.log(pwLength);
-    console.log(Number(pwLength));
   }
 
+  //Enable the user to select the type(s) of characters to include and validate the selection(s)
   while (lowerCase === false && upperCase === false && numeric === false && special === false) {
     lowerCase = confirm("Click OK to include lower case letters in your password; otherwise click Cancel.");
     upperCase = confirm("Click OK to include upper case letters in your password; otherwise click Cancel.");
@@ -45,4 +44,32 @@ function generatePassword() {
       alert("You must select at least one type of character to include in your password.");
     }
   }
-}
+
+  //Assemble array of characters to allow in password
+  let allCharacters = [];
+
+  if (lowerCase) {
+    for (let i = 0; i < lcLetters.length; i++) {
+      allCharacters.push(lcLetters[i]);
+    }
+  }
+
+  if (upperCase) {
+    for (let i = 0; i < ucLetters.length; i++) {
+      allCharacters.push(ucLetters[i]);
+    }
+  }
+
+  if (numeric) {
+    for (let i = 0; i < nums.length; i++) {
+      allCharacters.push(nums[i]);
+    }
+  }
+
+  if (special) {
+    for (let i = 0; i < specials.length; i++) {
+      allCharacters.push(specials[i]);
+    }
+  }
+  console.log(allCharacters);
+}  
